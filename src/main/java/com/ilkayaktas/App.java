4a8bf6abe994de7425166ec4fab5ce73ec6446ec
İlkay Aktaş <ilkayaktas@gmail.com>
2019-01-28 09:32:16 +0300

    Project is initialized.

---
+++ src/main/java/com/ilkayaktas/App.java
@@ -0,0 +1,11 @@
+package com.ilkayaktas;
+
+/**
+ * Created by aselsan on 28.01.2019 at 09:27.
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
